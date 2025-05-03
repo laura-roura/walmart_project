161df9acb35824ab5dc21abbac9cc19f0e1d1e4f
Laura Roura <laura.roura2201@gmail.com>
2025-05-03 19:28:10 +0000

    Loading the departments.csv table from S3.

---
+++ macros/copy_data_from_s3__departments.sql
@@ -0,0 +1,18 @@
+{% macro macros_copy_departments_csv(table_nm) %} 
+
+COPY INTO {{var ('raw_db') }}.{{var ('wrk_schema')}}.{{ table_nm }} 
+FROM 
+(
+SELECT
+    $1 AS Store_Id,
+    $2 AS Dept_Id,
+    $3 AS Department_Date,
+    $4 AS Weekly_Sales,
+    $5 AS IsHoliday,
+    CURRENT_TIMESTAMP() as CREATED_AT
+FROM @{{ var('stage_name') }}department.csv
+)
+FILE_FORMAT = {{var ('file_format_csv') }}
+PURGE={{ var('purge_status') }};
+
+{% endmacro %}
