4d6c159867068b33d5e6a31b59912b9a107aec28
Laura Roura <laura.roura2201@gmail.com>
2025-05-07 01:20:01 +0000

    Delte temporary tables that were not needed....

--- models/silver/walmart_store_dim.sql
+++ models/silver/walmart_store_dim.sql
@@ -8,9 +8,38 @@
     )
 }}
 
-with temp_store_dim as
+with 
+stores_src as
 (
+    select 
+        Store_Id,
+        Store_Type,
+        Store_Size,
+        CREATED_AT    
+    from {{ ref('stores') }}
+),
 
+departments_src as
+(
+    select
+        DISTINCT Store_Id, Dept_Id
+    from {{ ref('departments') }}
+    order by 1,2
+),
+
+stores_transformed as
+(
+    select 
+        a.Store_Id,
+        b.Dept_Id,
+        a.Store_Type,
+        a.Store_Size,
+        a.CREATED_AT
+        from stores_src a join departments_src b on (a.Store_Id = b.Store_Id)        
+),
+
+temp_store_dim as
+(
     select 
         Store_Id,
         Dept_Id,
@@ -19,7 +48,7 @@ with temp_store_dim as
         CREATED_AT,
         CURRENT_TIMESTAMP as INSERT_DTS,
         CURRENT_TIMESTAMP as UPDATE_DTS
-    from {{ ref('temp_store_dim') }}
+    from stores_transformed
 
      {% if is_incremental() %}
     where CREATED_AT > (select max(INSERT_DTS) from {{ this }} )
